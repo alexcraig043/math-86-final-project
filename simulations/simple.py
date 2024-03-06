83e1a36c8dd0206cdbd571c93100bd225dbe00b7
alexcraig <alexcraig043@gmail.com>
2024-03-06 16:24:29 -0500

    minor changes

--- simulations/simple.py
+++ simulations/simple.py
@@ -4,31 +4,6 @@ class Simple:
         self.y = y  # Quantity of asset y
         self.k = x * y  # Constant product
 
-    def add_liquidity(self, delta_x, delta_y):
-        """
-        Add liquidity to the pool.
-
-        :param delta_x: Amount of x added to the pool.
-        :param delta_y: Amount of y added to the pool.
-        """
-        self.x += delta_x
-        self.y += delta_y
-        self.k = self.x * self.y  # Recalculate constant product
-
-    def remove_liquidity(self, delta_x, delta_y):
-        """
-        Remove liquidity from the pool.
-
-        :param delta_x: Amount of x removed from the pool.
-        :param delta_y: Amount of y removed from the pool.
-        """
-        if delta_x <= self.x and delta_y <= self.y:
-            self.x -= delta_x
-            self.y -= delta_y
-            self.k = self.x * self.y  # Recalculate constant product
-        else:
-            print("Cannot remove more liquidity than available")
-
     def swap_x_for_y(self, delta_x):
         """
         Swap x for y using the AMM model.
